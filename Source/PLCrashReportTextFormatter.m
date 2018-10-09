9b46b436ce3fb843af2316b1500edcca2138fa00
Daniel Jalkut <jalkut@red-sweater.com>
2018-10-09 08:43:11 -0400

    There doesn't seem to be any merit in including a "TODO" line in the crash log.

--- Source/PLCrashReportTextFormatter.m
+++ Source/PLCrashReportTextFormatter.m
@@ -184,7 +184,6 @@ static NSInteger binaryImageSort(id binary1, id binary2, void *context);
         }
     
         [text appendFormat: @"Incident Identifier: %@\n", incidentIdentifier];
-        [text appendFormat: @"CrashReporter Key:   TODO\n"];
         [text appendFormat: @"Hardware Model:      %@\n", hardwareModel];
     }
     
